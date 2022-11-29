2cd24426cc1325e328ff2f9ad2e3c3a228e01cc0
Nicolas Kawahala <nickmaskaw@gmail.com>
2022-11-29 11:29:41 -0300

    Update thzcontrol.py

--- thzcontrol.py
+++ thzcontrol.py
@@ -6,11 +6,6 @@ from PyQt6.QtCore import QTimer
 from interface import MainWindow, LeftDockWidget, ConnectionContainer, ControlContainer, LivePlot
 from instruments import VISAInstrument, LockIn, KBD101, OttimeDelayline, Cernox
 
-def update():
-    global liveplot, timer, i, t, X
-    i += 1
-    liveplot.data_line.setData(x=t[:i], y=X[:i])
-    if i == len(t): timer.stop()
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -25,20 +20,7 @@ if __name__ == '__main__':
     connection_widget = LeftDockWidget("Connection", main_window, ConnectionContainer(lockin, cernox, pmp_dl, thz_dl))
     control_widget    = LeftDockWidget("Manual Control", main_window, ControlContainer(cernox, pmp_dl, thz_dl))
     
-    liveplot = LivePlot(main_window)
-    
-    data = pd.read_table("sample_signal.dat")
-    t = data['t'].values
-    X = data['X'].values
-    
-    i = 0
-    
-    timer = QTimer()
-    timer.setInterval(50)
-    timer.timeout.connect(update)
-    timer.start()
-    
-    
+    liveplot = LivePlot(main_window)  
     
     main_window.show()
     
