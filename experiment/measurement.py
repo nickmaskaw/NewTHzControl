22312a282e7e2db39eda76050f1a4dfb7e4d4fa2
Nicolas Kawahala <nickmaskaw@gmail.com>
2022-12-12 11:43:24 -0300

    Implement repeat loop

--- experiment/measurement.py
+++ experiment/measurement.py
@@ -4,6 +4,7 @@ import numpy as np
 from pandas import DataFrame
 from PyQt6.QtCore import QObject, QThreadPool, QRunnable, pyqtSignal, pyqtSlot
 
+
 class Constants:
     C = 299_792_458e3/1e12  # mm/ps (~0.3mm/ps)
     
@@ -57,8 +58,52 @@ class Measurement:
                 os.makedirs(folder)
                 print(f"Created {folder} folder")
         print("Measurement output folders OK")
+
+    def scan(self):
+        thz_start = float(self.parameters.mandatory.thz_start.value)
+        thz_end   = float(self.parameters.mandatory.thz_end.value)
+        thz_step  = float(self.parameters.mandatory.thz_step.value) 
+        thz_vel   = float(self.parameters.mandatory.thz_vel.value)
+        
+        pmp_start = float(self.parameters.mandatory.pmp_start.value)
+        pmp_end   = float(self.parameters.mandatory.pmp_end.value)
+        pmp_step  = float(self.parameters.mandatory.pmp_step.value) 
+        pmp_vel   = float(self.parameters.mandatory.pmp_vel.value)
+        
+        tcons     = float(self.parameters.hidden.tcons.value) 
+        wait      = float(self.parameters.mandatory.wait.value)
+        plot_rate = int(self.parameters.mandatory.plot_rate.value)
+        repeat    = int(self.parameters.unsavable.repeat.value)
+        
+        
+        for rep in range(repeat):
+            self.thz_dl.returnTo(thz_start)
+            self.pmp_dl.returnTo(pmp_start)
+            self.thz_dl.startPolling(10)
+            self.thz_dl.setVelocity(thz_vel)
+            self.pmp_dl.setVelocity(pmp_vel)
+            tm.sleep(10 * tcons)
+        
+            N   = int( abs(thz_end - thz_start) / thz_step ) + 1
+            pos = np.linspace(thz_start, thz_end, N)
+            X   = np.full(N, np.nan)
             
-    def thzScan(self):
+            for i in range(N):
+                self.thz_dl.moveTo(pos[i])
+                tm.sleep(wait * tcons)
+                X[i] = self.lockin.X()
+                if i% plot_rate == 0:
+                    self.signals.updated.emit(pos, X)
+                    
+                if self.break_:
+                    self.break_ = False
+                    break
+                    
+            self.thz_dl.stopPolling()
+            self.signals.finished.emit()
+            print(DataFrame({'pos': pos, 'X': X}))  
+            
+    def thzScan_(self):
         thz_start = float(self.parameters.mandatory.thz_start.value)
         thz_end   = float(self.parameters.mandatory.thz_end.value)
         thz_step  = float(self.parameters.mandatory.thz_step.value) 
@@ -130,5 +175,5 @@ class Measurement:
     @pyqtSlot()
     def run(self):
         pool = QThreadPool.globalInstance()
-        worker = MeasurementWorker(self.thzScan)
+        worker = MeasurementWorker(self.scan)
         pool.start(worker)
